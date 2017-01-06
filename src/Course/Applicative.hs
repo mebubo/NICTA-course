7231a354d5c9e695d4c0bd92c48881fc5ca9c031
Sergei Dolgov <dolgovs@gmail.com>
2017-01-06 09:57:40 +0100

    Applicative wip

--- src/Course/Applicative.hs
+++ src/Course/Applicative.hs
@@ -125,8 +125,8 @@ instance Applicative Optional where
     Optional (a -> b)
     -> Optional a
     -> Optional b
-  (<*>) =
-    error "todo: Course.Apply (<*>)#instance Optional"
+  f <*> a =
+    bindOptional (\f' -> mapOptional (\a' -> f' a') a) f
 
 -- | Insert into a constant function.
 --
@@ -151,13 +151,14 @@ instance Applicative ((->) t) where
     a
     -> ((->) t a)
   pure =
-    error "todo: Course.Applicative pure#((->) t)"
+    const
   (<*>) ::
     ((->) t (a -> b))
     -> ((->) t a)
     -> ((->) t b)
-  (<*>) =
-    error "todo: Course.Apply (<*>)#instance ((->) t)"
+  f <*> g =
+    \t -> f t $ g t
+
 
 
 -- | Apply a binary function in the environment.
@@ -185,8 +186,8 @@ lift2 ::
   -> f a
   -> f b
   -> f c
-lift2 =
-  error "todo: Course.Applicative#lift2"
+lift2 f a b =
+  f <$> a <*> b
 
 -- | Apply a ternary function in the environment.
 --
@@ -217,8 +218,8 @@ lift3 ::
   -> f b
   -> f c
   -> f d
-lift3 =
-  error "todo: Course.Applicative#lift3"
+lift3 f a b c =
+  f <$> a <*> b <*> c
 
 -- | Apply a quaternary function in the environment.
 --
@@ -250,8 +251,8 @@ lift4 ::
   -> f c
   -> f d
   -> f e
-lift4 =
-  error "todo: Course.Applicative#lift4"
+lift4 f a b c d =
+  f <$> a <*> b <*> c <*> d
 
 -- | Apply, discarding the value of the first argument.
 -- Pronounced, right apply.
@@ -277,7 +278,8 @@ lift4 =
   -> f b
   -> f b
 (*>) =
-  error "todo: Course.Applicative#(*>)"
+  flip (<*)
+
 
 -- | Apply, discarding the value of the second argument.
 -- Pronounced, left apply.
@@ -302,8 +304,8 @@ lift4 =
   f b
   -> f a
   -> f b
-(<*) =
-  error "todo: Course.Applicative#(<*)"
+fb <* fa =
+  const <$> fb <*> fa
 
 -- | Sequences a list of structures to a structure of list.
 --
